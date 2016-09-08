86e48f2a4b4842ce08c7c102f3c931651540d659
Yan Kozlovskiy <yankozlovskiy@icloud.com>
2016-09-08 17:04:51 +0000

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,22 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
+  return document.querySelectorAll("#nested .target")[0];
+}
 
+function deepestChild() {
+  var element = document.querySelectorAll("#grand-node");
+  for (var i = 0; i < element.length; i++) {
+    if (i === element.length - 1) {
+      return element[i];
+    }
+  }
+}
+
+function increaseRankBy(n) {
+  var list = document.querySelectorAll(".ranked-list li")[0];
+  for (let i = 0; i < list.length; i++) {
+    list[i] = list[i] + n;
+  }
+  return list;
 }
